a2726e371208dbbb0ffcb94261f9f7cbea1db233
William <69988679+Killaship@users.noreply.github.com>
2023-04-10 14:33:19 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 g = 6.673 * (10**-11) # Gravitational constant
 
+bodies = {"body1": {"mass": 100, "pos": [1, 0]},   }
+
 def calcforce(mass1, mass2, distance):
     return (g * mass1 * mass2) / distance ** 2 # Fg = (G*M1*M2)/D^2
 
